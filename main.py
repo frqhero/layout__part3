efac7dc568fc7604522acf33f4214760d1b4edf1
frqhero <frqhero@gmail.com>
2023-05-14 19:30:45 +0600

    Download book covers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from os.path import join
+from urllib.parse import urljoin, urlparse, urlsplit, unquote
 import os
 
 from bs4 import BeautifulSoup
@@ -23,6 +24,16 @@ def download_txt(url, filename, folder='books/'):
     with open(file_path, 'wb') as file:
         file.write(response.content)
 
+def download_image(counter, img_url):
+    base = 'https://tululu.org'
+    link = urljoin(base, img_url)
+    response = requests.get(link)
+    response.raise_for_status()
+    os.makedirs('images', exist_ok=True)
+    filename = urlsplit(unquote(link)).path.split('/')[-1]
+    with open(f'images/{filename}', 'wb') as file:
+        file.write(response.content)
+
 
 def get_title_author(url):
     response = requests.get(url)
@@ -35,13 +46,25 @@ def get_title_author(url):
     return title, author
 
 
+def print_book_image_link(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+    html_page_content = response.text
+    soup = BeautifulSoup(html_page_content, 'lxml')
+    return soup.find(class_='bookimage').find('a').find('img').attrs['src']
+
+
+
 def main():
     for counter in range(1, 11):
         try:
             page_url = f'https://tululu.org/b{counter}/'
             title, author = get_title_author(page_url)
-            book_url = f'http://tululu.org/txt.php?id={counter}'
-            download_txt(book_url, title)
+            img_url = print_book_image_link(page_url)
+            download_image(counter, img_url)
+            # book_url = f'http://tululu.org/txt.php?id={counter}'
+            # download_txt(book_url, title)
         except Exception as e:
             print(e)
 
