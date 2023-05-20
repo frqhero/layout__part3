283691f75b671b94e47c5bb1271b164983bdb445
frqhero <frqhero@gmail.com>
2023-05-20 21:22:59 +0600

    Reduce Qty of requests

--- main.py
+++ main.py
@@ -13,33 +13,7 @@ def check_for_redirect(response):
         raise requests.exceptions.HTTPError('Книга не найдена')
 
 
-def download_txt(url, filename, folder='books/'):
-    folder = sanitize_filepath(folder)
-    os.makedirs(folder, exist_ok=True)
-    response = requests.get(url)
-    response.raise_for_status()
-    check_for_redirect(response)
-    filename = sanitize_filename(filename)
-    file_path = join(folder, f'{filename}.txt')
-    with open(file_path, 'wb') as file:
-        file.write(response.content)
-
-def download_image(counter, img_url):
-    base = 'https://tululu.org'
-    link = urljoin(base, img_url)
-    response = requests.get(link)
-    response.raise_for_status()
-    check_for_redirect(response)
-    os.makedirs('images', exist_ok=True)
-    filename = urlsplit(unquote(link)).path.split('/')[-1]
-    with open(f'images/{filename}', 'wb') as file:
-        file.write(response.content)
-
-
-def get_title_author(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    check_for_redirect(response)
+def get_title_author(response):
     html_page_content = response.text
     soup = BeautifulSoup(html_page_content, 'lxml')
     post_title = soup.find('h1').text
@@ -47,18 +21,23 @@ def get_title_author(url):
     return title, author
 
 
-def print_book_image_link(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    check_for_redirect(response)
+def get_book_image_link(response):
     html_page_content = response.text
     soup = BeautifulSoup(html_page_content, 'lxml')
     return soup.find(class_='bookimage').find('a').find('img').attrs['src']
 
-def download_comments(url):
-    response = requests.get(url)
+
+def download_image(img_url):
+    response = requests.get(img_url)
     response.raise_for_status()
     check_for_redirect(response)
+    os.makedirs('images', exist_ok=True)
+    filename = urlsplit(unquote(img_url)).path.split('/')[-1]
+    with open(f'images/{filename}', 'wb') as file:
+        file.write(response.content)
+
+
+def download_comments(response):
     html_page_content = response.text
     soup = BeautifulSoup(html_page_content, 'lxml')
     comments = [
@@ -70,16 +49,33 @@ def download_comments(url):
     print()
 
 
+def download_txt(url, filename, folder='books/'):
+    folder = sanitize_filepath(folder)
+    os.makedirs(folder, exist_ok=True)
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+    filename = sanitize_filename(filename)
+    file_path = join(folder, f'{filename}.txt')
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
+
+
 def main():
     for counter in range(1, 11):
         try:
-            page_url = f'https://tululu.org/b{counter}/'
-            title, author = get_title_author(page_url)
+            base = 'https://tululu.org'
+            book_url = urljoin(base, f'b{counter}')
+            response = requests.get(book_url)
+            response.raise_for_status()
+            check_for_redirect(response)
+            title, author = get_title_author(response)
             print(title, sep='\n')
-            img_url = print_book_image_link(page_url)
-            download_image(counter, img_url)
-            download_comments(page_url)
-            # book_url = f'http://tululu.org/txt.php?id={counter}'
+            # img_path = get_book_image_link(response)
+            # img_url = urljoin(base, img_path)
+            # download_image(img_url)
+            download_comments(response)
+            # book_url = urljoin(base, f'txt.php?id={counter}')
             # download_txt(book_url, title)
         except Exception as e:
             print(e)
