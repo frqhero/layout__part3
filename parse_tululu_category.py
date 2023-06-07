700a56e0b5ac2622f417f7df7ee2ea2761dc0200
yaroslav <frqhero@gmail.com>
2023-06-07 14:32:41 +0600

    Download books from all pages

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,7 +1,15 @@
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlsplit
+import json
 
 from bs4 import BeautifulSoup
 import requests
+
+from main import (
+    check_for_redirect,
+    parse_book_page,
+    download_image,
+    download_txt,
+)
 
 
 def parse_books_by_page_link(netloc, link):
@@ -10,18 +18,37 @@ def parse_books_by_page_link(netloc, link):
     soup = BeautifulSoup(response.text, 'lxml')
     content = soup.find('div', {'id': 'content'})
     links = [
-        urljoin(netloc, x.find('a')['href'])
-        for x in content.find_all('table')
+        urljoin(netloc, table.find('a')['href']).rstrip('/')
+        for table in content.find_all('table')
     ]
     return links
 
 
 def main():
     netloc = 'https://tululu.org/'
-    for counter in range(1, 11):
+    links = []
+    for counter in range(1, 5):
         sci_fi_page_address = urljoin(netloc, f'l55/{counter}')
-        links = parse_books_by_page_link(netloc, sci_fi_page_address)
-        print("\n".join(links))
+        links += parse_books_by_page_link(netloc, sci_fi_page_address)
+
+    # print("\n".join(links))
+    books_description = []
+    for book_url in links:
+        try:
+            response = requests.get(book_url)
+            response.raise_for_status()
+            check_for_redirect(response.url)
+
+            parsed_book_page = parse_book_page(response.text)
+            book_id = urlsplit(book_url).path.replace('/', '').replace('b', '')
+
+            download_txt(book_url, book_id, parsed_book_page['title'])
+            download_image(book_url, parsed_book_page['img_address'])
+            books_description.append(parsed_book_page)
+        except requests.exceptions.HTTPError as e:
+            print(e)
+    with open('book_description.json', 'w', encoding='utf8') as json_file:
+        json.dump(books_description, json_file, ensure_ascii=False)
 
 
 if __name__ == '__main__':
