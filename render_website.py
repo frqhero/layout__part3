efeb7be3fbefa90ec96da205b3d4d145b3523cc0
Yaroslav Voroshilov <frqhero@gmail.com>
2023-07-05 09:16:45 +0600

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,34 +1,38 @@
 import json
+import os
 
 from livereload import Server
 from more_itertools import chunked
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def prepare_html():
+def prepare_html(books_description, counter):
+    folder = 'pages'
+    os.makedirs(folder, exist_ok=True)
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
 
-    with open('books_description.json', 'r') as file:
-        books_description = json.load(file)
-
     for book in books_description:
-        book['address'] = f'books/{book["title"]}.txt'
+        book['address'] = f'../books/{book["title"]}.txt'
 
-    books_description = list(chunked(books_description, 2))
+    books_description_by_2 = list(chunked(books_description, 2))
 
+    rendered_page = template.render(books_description=books_description_by_2)
 
-    rendered_page = template.render(books_description=books_description)
-
-    with open('index.html', 'w', encoding="utf8") as file:
+    with open(f'{folder}/index{counter}.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
 
 def main():
-    prepare_html()
+    with open('books_description.json', 'r') as file:
+        books_description_content = json.load(file)
+
+    books_description_by_10 = list(chunked(books_description_content, 10))
+    for counter, books_description in enumerate(books_description_by_10, 1):
+        prepare_html(books_description, counter)
 
     server = Server()
     server.watch('template.html', prepare_html)
