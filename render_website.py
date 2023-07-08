d4b5090b9f5cd5d5e3e38f42fdaae283727a473b
Yaroslav Voroshilov <frqhero@gmail.com>
2023-07-08 19:11:34 +0600

    Change var name: Singular -> Plural

--- render_website.py
+++ render_website.py
@@ -7,7 +7,7 @@ from more_itertools import chunked
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def prepare_page(books_description, counter, page_count):
+def prepare_page(book_descriptions, counter, page_count):
     folder = 'pages'
     os.makedirs(folder, exist_ok=True)
     env = Environment(
@@ -16,12 +16,12 @@ def prepare_page(books_description, counter, page_count):
     )
     template = env.get_template('template.html')
 
-    for book in books_description:
+    for book in book_descriptions:
         book['address'] = f'../books/{book["title"]}.txt'
         pic_name = os.path.basename(book['img_address'])
         book['img_address'] = f'../media/images/{pic_name}'
 
-    books_description_by_2 = list(chunked(books_description, 2))
+    books_description_by_2 = list(chunked(book_descriptions, 2))
 
     rendered_page = template.render(
         books_description=books_description_by_2,
@@ -42,8 +42,8 @@ def prepare_html():
     page_count = math.ceil(len(books_description_content) / 10)
 
     books_description_by_10 = list(chunked(books_description_content, 10))
-    for counter, books_description in enumerate(books_description_by_10, 1):
-        prepare_page(books_description, counter, page_count)
+    for counter, book_descriptions in enumerate(books_description_by_10, 1):
+        prepare_page(book_descriptions, counter, page_count)
 
 
 def main():
